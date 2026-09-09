8d908fa7815c8f3d67e9c9ec08a5851c8745fffd
Luiz Augusto Zaffalon <lazaffalon@gmail.com>
2026-09-09 18:32:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 
 st.title("Meu Primeiro Aplicativo no Ar!")
-st.write("Criado por mim e pronto para o Streamlit".)
+st.write("Criado por mim e pronto para o Streamlit")
 
 if st.button("Clique aqui para testar")
 st.sucess("Parabéns! O seu código funcionou de primeira!")
