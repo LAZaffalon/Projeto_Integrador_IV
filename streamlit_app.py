097c068fb420c9eaa7bbb9b4d233b1697ae3ad66
Luiz <lazaffalon@gmail.com>
2026-09-09 22:16:44 -0300

    Update streamlit_app.py
    Edição local do arquivo streamlit.py e posterior commit para o repositório do projeto.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
+#Importar as bibliotecas para a aplicação
 import streamlit as st 
 
+#Carregar os datasets tratados para executar a análise descritiva
+
+#Preparar as visiualizações
+
+#Criar a interface no Streamlit
+
 st.title("Meu Primeiro Aplicativo no Ar!")
-st.write("Criado por mim e pronto para o Streamlit")
+st.write("Esta é uma alteração feita diretamente no VSCode")
 
 if st.button("Clique aqui para testar"):
-  st.success("Parabéns! O seu código funcionou de primeira!")
+    st.success("Parabéns! O seu código funcionou de primeira!")
